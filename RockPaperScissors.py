f5cfe5b5adac8f54e200d5f43d239262f060240a
Aga <60896654+agakarnas@users.noreply.github.com>
2022-01-28 22:10:44 +0100

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,66 +1,87 @@
 import sys 
 import random
 
-print('ROCK, PAPER, SCISSORS')
+print('KAMIEŃ, PAPIER, NOŻYCE')
 
-wins = 0
-losses = 0
-ties = 0
+print()
+print()
+
+zwyciestwo = 0
+porazka = 0
+remis = 0
 
 while True:
-    print('%s Wins, %s Losses, %s Ties' %(wins, losses, ties))
+    print('%s Zwycięstw, %s Porażek, %s Remisów' %(zwyciestwo, porazka, remis))
+    print()
     while True:
-        print('Enter your move: (r)ock, (p)aper, (s)cissors or (q)uit')
-        yourMove = input()
-        if yourMove == 'q':
+        print('Co wybierasz: (k)amień, (p)apier, (n)ożyce or (w)yjdź ?')
+        TwojRuch = input()
+        if TwojRuch == 'w':
             sys.exit()
-        if yourMove == 'r' or yourMove == 'p' or yourMove == 's':
+        if TwojRuch == 'k' or TwojRuch == 'p' or TwojRuch == 'n':
             break
-        print('Type one of r, p, s or q.')
+        print()
+        print('Wpisz k, p, n lub w.')
+        print()
+        print()
+        
+    if TwojRuch == 'k':
+        print('Kamień kontra ...')
+    elif TwojRuch == 'p':
+        print('Papier kontra ...')
+    elif TwojRuch == 'n':
+        print('Nożyce kontra ...')
     
-    if yourMove == 'r':
-        print('Rock vs. ...')
-    elif yourMove == 'p':
-        print('Paper vs ...')
-    elif yourMove == 's':
-        print('Scissors vs. ...')
-    
-    RandNum = random.randint(1,3)
+    RandLiczba = random.randint(1,3)
 
-    if RandNum == 1:
-        computermove = 'r'
-        print('Rock')
-    elif RandNum == 2:
-        compMove = 'p'
-        print('Paper')
-    elif RandNum == 3:
-        compMove = 's'
-        print('Scissors')
+    if RandLiczba == 1:
+        RuchKomputer = 'k'
+        print('KAMIEŃ')
+    elif RandLiczba == 2:
+        RuchKomputer = 'p'
+        print('PAPIER')
+    elif RandLiczba == 3:
+        RuchKomputer = 'n'
+        print('NOŻYCE')
     
-    if yourMove == compMove:
-        print("It's a tie!")
-        ties = ties + 1
+    if TwojRuch == RuchKomputer:
+        print()
+        print("Remis!")
+        print()
+        remis = remis + 1
     
-    elif yourMove == 'r' and compMove == 's':
-        print('You won!')
-        wins = wins + 1
+    elif TwojRuch == 'k' and RuchKomputer == 'n':
+        print()
+        print('Brawo! Zwycięstwo!')
+        print()
+        zwyciestwo = zwyciestwo + 1
     
-    elif yourMove == 's' and compMove == 'p':
-         print('You won!')
-         wins = wins + 1   
+    elif TwojRuch == 'n' and RuchKomputer == 'p':
+         print() 
+         print('Brawo! Zwycięstwo!')
+         print()
+         zwyciestwo = zwyciestwo + 1   
          
-    elif yourMove == 'p' and compMove == 'r':
-        print('You won!')
-        wins = wins + 1
+    elif TwojRuch == 'p' and RuchKomputer == 'k':
+        print()
+        print('Zwycięstwo!')
+        print()
+        zwyciestwo = zwyciestwo + 1
     
-    elif yourMove == 'p' and compMove == 's':
-        print('You lost!')
-        losses = losses + 1
+    elif TwojRuch == 'p' and RuchKomputer == 'n':
+        print()
+        print('Niestety, nie tym razem!')
+        print()
+        porazka = porazka + 1
 
-    elif yourMove == 's' and compMove == 'r':
-        print('You lost!')
-        losses = losses + 1
+    elif TwojRuch == 'n' and RuchKomputer == 'k':
+        print()
+        print('Niestety, nie tym razem!!')
+        print()
+        porazka = porazka + 1
     
-    elif yourMove == 'r' and compMove == 'p':
-        print('You lost!')
-        losses = losses + 1
+    elif TwojRuch == 'k' and RuchKomputer == 'p':
+        print()
+        print('Niestety, nie tym razem!!')
+        print()
+        porazka = porazka + 1
